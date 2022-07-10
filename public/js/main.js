0035c77952598d4df6f9bc2d9cbd876b668eec60
deelaurence <laurie143pelumi@gmail.com>
2022-07-10 07:16:56 +0100

    updated css

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
@@ -11,140 +11,126 @@ const { username, room } = Qs.parse(location.search, {
 const socket = io();
 
 // Join chatroom
-socket.emit('joinRoom', { username, room });
+socket.emit("joinRoom", { username, room });
 
 // Get room and users
-socket.on('roomUsers', ({ room, users }) => {
+socket.on("roomUsers", ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
 
 // Message from server
-socket.on('message', (message) => {
+socket.on("message", (message) => {
   console.log(message);
   outputMessage(message);
 
- 
   // Scroll down
   chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message submit
-chatForm.addEventListener('submit', (e) => {
+chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
- 
+
   // Get message text
   let msg = e.target.elements.msg.value;
-  
+
   msg = msg.trim();
 
   if (!msg) {
     return false;
-
   }
 
   // Emit message to server
-  socket.emit('chatMessage', msg);
+  socket.emit("chatMessage", msg);
 
   // Clear input
-  e.target.elements.msg.value = '';
+  e.target.elements.msg.value = "";
   e.target.elements.msg.focus();
 });
 
+var msgOut = document.getElementById("message-out");
+var msgIn = document.getElementById("message-in");
+var keypad = document.getElementById("keypad");
 
+function playAudio() {
+  x.play();
+}
 
-var msgOut = document.getElementById("message-out"); 
-var msgIn = document.getElementById("message-in"); 
-var keypad = document.getElementById("keypad"); 
-
-function playAudio() { 
-  x.play(); 
-} 
-
-function pauseAudio() { 
-  x.pause(); 
-} 
+function pauseAudio() {
+  x.pause();
+}
 function playPad() {
-  keypad.play()
-  keypad.playbackRate=2;
+  keypad.play();
+  keypad.playbackRate = 2;
 }
 // Output message to DOM
 function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  const p = document.createElement('p');
-  p.classList.add('meta');
+  const div = document.createElement("div");
+  div.classList.add("message");
+  const p = document.createElement("p");
+  p.classList.add("meta");
   p.innerText = message.username;
   p.innerHTML += `<span>${message.time}</span>`;
   div.appendChild(p);
-  const para = document.createElement('p');
-  para.classList.add('text');
+  const para = document.createElement("p");
+  para.classList.add("text");
   para.innerText = message.text;
 
-
   div.appendChild(para);
-  
- let request= new XMLHttpRequest();
- request.addEventListener("load", function(evt){
-  //  console.log(evt);
-   if (this.readyState== 4 && this.status == 200){
-let hero =document.getElementById('username')
 
-// console.log(this.responseText);
-   }
-   })
+  let request = new XMLHttpRequest();
+  request.addEventListener("load", function (evt) {
+    //  console.log(evt);
+    if (this.readyState == 4 && this.status == 200) {
+      let hero = document.getElementById("username");
 
- request.open('GET', 'index.html', true),
- request.send()
-  document.querySelector('.chat-messages').appendChild(div);
-    console.log(message.username)
-     console.log(window.location.search);
-     let hasText = window.location.search.includes(message.username)
-if(hasText){
-  div.style.backgroundColor=" rgb(255, 225, 230)";
-  // div.style.marginLeft='65%';
-  div.style.position='relative';
-  div.style.left='100%';
-  div.style.borderTopLeftRadius='15px'
-  div.style.borderTopRightRadius='0'
-	div.style.transform='translateX(-100%)';
-  msgIn.play()
-}
-else if(message.username=='admin'){
-  div.style.position='relative'
-  div.style.backgroundColor="rgba(257,257,257,.7)";
-  div.style.left='50%'
-  div.style.width='50%'
-  div.style.textAlign='center'
-  div.style.transform='translateX(-50%)';
-  console.log('admin');
-}
-else{
-  msgOut.play()
+      // console.log(this.responseText);
+    }
+  });
+
+  request.open("GET", "index.html", true), request.send();
+  document.querySelector(".chat-messages").appendChild(div);
+  console.log(message.username);
+  console.log(window.location.search);
+  let hasText = window.location.search.includes(message.username);
+  if (hasText) {
+    div.style.backgroundColor = " rgb(255, 225, 230)";
+    // div.style.marginLeft='65%';
+    div.style.position = "relative";
+    div.style.left = "100%";
+    div.style.borderTopLeftRadius = "15px";
+    div.style.borderTopRightRadius = "0";
+    div.style.transform = "translateX(-100%)";
+    msgIn.play();
+  } else if (message.username == "admin") {
+    div.style.position = "relative";
+    div.style.backgroundColor = "rgba(257,257,257,.7)";
+    div.style.left = "50%";
+    div.style.width = "50%";
+    div.style.textAlign = "center";
+    div.style.transform = "translateX(-50%)";
+    console.log("admin");
+  } else {
+    msgOut.play();
+  }
 }
 
-}
+let muteBtn = document.querySelector(".mute");
+let muteInfo = document.querySelector(".mute-info");
 
-let muteBtn = document.querySelector('.mute')
-let muteInfo=document.querySelector('.mute-info')
+muteBtn.addEventListener("click", () => {
+  muteBtn.style.color = "#63bb63";
+  msgIn.muted = true;
+  msgOut.muted = true;
+});
 
-muteBtn.addEventListener('click',()=>{
-  muteBtn.style.color='#63bb63'
-  msgIn.muted=true;
-  msgOut.muted=true;
-  muteInfo.innerHTML="double click to unmute"
-  
-})
-
-muteBtn.addEventListener('dblclick',()=>{
-  muteBtn.style.color='white'
-  muteInfo.innerHTML=''
-  msgIn.muted=false;
-  msgOut.muted=false;
-})
-
-
-
+muteBtn.addEventListener("dblclick", () => {
+  muteBtn.style.color = "white";
+  muteInfo.innerHTML = "";
+  msgIn.muted = false;
+  msgOut.muted = false;
+});
 
 // Add room name to DOM
 function outputRoomName(room) {
@@ -153,30 +139,30 @@ function outputRoomName(room) {
 
 // Add users to DOM
 function outputUsers(users) {
-  userList.innerHTML = '';
+  userList.innerHTML = "";
   users.forEach((user) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.innerText = user.username;
     userList.appendChild(li);
   });
 }
 
 //Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+document.getElementById("leave-btn").addEventListener("click", () => {
+  const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
   if (leaveRoom) {
-    window.location = '../index.html';
+    window.location = "../index.html";
   } else {
   }
 });
 
-
-let info = document.querySelector('.info')
-let sideBar = document.querySelector('.chat-sidebar')
-let body = document.querySelector('#blur-bg')
-info.addEventListener('click',()=>{
-body.classList.toggle('blur')
-document.getElementById('send-btn').classList.toggle('blur')
-sideBar.classList.toggle('chat-sidebar2')
-})
-
+let info = document.querySelector(".info");
+let sideBar = document.querySelector(".chat-sidebar");
+let body = document.querySelector("#blur-bg");
+info.addEventListener("click", () => {
+  body.classList.toggle("blur");
+  document.querySelector(".chat-messages").classList.toggle("blur");
+  document.querySelector(".message").classList.toggle("fade");
+  document.getElementById("send-btn").classList.toggle("blur");
+  sideBar.classList.toggle("chat-sidebar2");
+});
--- server.js
+++ server.js
@@ -1,101 +1,91 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 const {
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers
-} = require('./utils/users');
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'admin';
+const botName = "admin";
 
+// Run when client connects
 
-   
-    
-  // Run when client connects
-
-
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   try {
-  socket.on('joinRoom', ({ username, room }) => {
-    const user = userJoin(socket.id, username, room);
+    socket.on("joinRoom", ({ username, room }) => {
+      const user = userJoin(socket.id, username, room);
 
-    socket.join(user.room);
+      socket.join(user.room);
 
-    // Welcome current user
-    socket.emit('message', formatMessage(botName, `you logged in as ${username}`));
-
-    // Broadcast when a user connects
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        'message',
-        formatMessage(botName, `${user.username} has joined the chat`)
+      // Welcome current user
+      socket.emit(
+        "message",
+        formatMessage(botName, `you logged in as ${username}`)
       );
 
-    // Send users and room info
-    io.to(user.room).emit('roomUsers', {
-      room: user.room,
-      users: getRoomUsers(user.room)
+      // Broadcast when a user connects
+      socket.broadcast
+        .to(user.room)
+        .emit(
+          "message",
+          formatMessage(botName, `${user.username} has joined the chat`)
+        );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
     });
-  });
-  
   } catch (error) {
     console.log(error);
   }
-  
-  // Listen for chatMessage
-  socket.on('chatMessage', msg => {
-   try {
-    const user = getCurrentUser(socket.id);
 
-    io.to(user.room).emit('message', formatMessage(user.username, msg));
-    
-   } catch (error) {
-     console.log(error);
-   } 
-      });
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    try {
+      const user = getCurrentUser(socket.id);
+
+      io.to(user.room).emit("message", formatMessage(user.username, msg));
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
   // Runs when client disconnects
-  socket.on('disconnect', () => {
-   
-   try {
-    const user = userLeave(socket.id);
+  socket.on("disconnect", () => {
+    try {
+      const user = userLeave(socket.id);
 
-    if (user) {
-      io.to(user.room).emit(
-        'message',
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
+      if (user) {
+        io.to(user.room).emit(
+          "message",
+          formatMessage(botName, `${user.username} has left the chat`)
+        );
 
-      // Send users and room info
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room)
-      });
-    } 
-   } catch (error) {
-     console.log(error);
-   }
-    
+        // Send users and room info
+        io.to(user.room).emit("roomUsers", {
+          room: user.room,
+          users: getRoomUsers(user.room),
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
-    
-    
-   
 
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
